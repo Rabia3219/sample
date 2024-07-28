3d22ad156fd2624d1a9661a3448538ff3b40050a
Rabia3219 <a321944@gmail.com>
2024-07-28 15:48:34 -0400

    db.sql add

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Employee (
+    EmployeeID INT  PRIMARY KEY,
+    FirstName VARCHAR (50),
+    LastName VARCHAR (50),
+    BirthDate DATE, 
+    Position VARCHAR (50),
+    Salary DECIMAL (10, 2)
+);
